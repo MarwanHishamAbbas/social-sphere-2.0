7db7d12d0ef9e019c60280cf7da2ead4f5b28858
Marwan Hisham <marwanhiisham@outlook.com>
2023-05-23 23:34:17 +0300

    before adding query logic

--- src/components/Post/CreatePostForm.tsx
+++ src/components/Post/CreatePostForm.tsx
@@ -1,9 +1,29 @@
 import { useUser } from "@clerk/nextjs";
 import { Avatar, Button, Card, Group, Textarea } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { Send } from "lucide-react";
+import { api } from "~/utils/api";
 
 function CreatePostForm() {
   const { user } = useUser();
+
+  const form = useForm({
+    initialValues: {
+      content: "",
+    },
+  });
+
+  const createPost = api.post.create.useMutation();
+  const createPostHandler = () => {
+    createPost.mutate(
+      { postContent: form.values.content },
+      {
+        onSuccess: () => {
+          form.reset();
+        },
+      }
+    );
+  };
 
   return (
     <Card radius={"md"} className="mt-7 lg:p-7">
@@ -15,6 +35,7 @@ function CreatePostForm() {
             alt={user?.username as string}
           />
           <Textarea
+            {...form.getInputProps("content")}
             className="flex-grow"
             radius={"xl"}
             placeholder="What's in you mind?"
@@ -23,6 +44,7 @@ function CreatePostForm() {
         </Group>
         <Group position="right">
           <Button
+            onClick={createPostHandler}
             leftIcon={<Send size={20} />}
             className="w-full lg:w-auto"
             mt={20}
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,20 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  // hello: publicProcedure
-  //   .input(z.object({ text: z.string() }))
-  //   .query(({ input }) => {
-  //     return {
-  //       greeting: `Hello ${input.text}`,
-  //     };
-  //   }),
-  // getAll: publicProcedure.query(({ ctx }) => {
-  //   return ctx.prisma.post.create({data: })
-  // }),
+  create: protectedProcedure
+    .input(z.object({ postContent: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const currentUserId = ctx.auth.userId;
+      const currentUserData = await clerkClient.users.getUser(currentUserId);
+
+      const exsitingUser = await ctx.prisma.user.findUnique({
+        where: {
+          email: currentUserData.emailAddresses[0]?.emailAddress,
+        },
+      });
+      if (exsitingUser) {
+        console.log(exsitingUser.email);
+      } else {
+        await ctx.prisma.user.create({
+          data: {
+            id: currentUserData.id,
+            email: currentUserData.emailAddresses[0]?.emailAddress as string,
+            profileImage: currentUserData.profileImageUrl,
+            username: currentUserData.username as string,
+          },
+        });
+      }
+      const createPost = await ctx.prisma.post.create({
+        data: {
+          postContent: input.postContent,
+          userId: ctx.auth.userId,
+        },
+      });
+      return createPost;
+    }),
 });
